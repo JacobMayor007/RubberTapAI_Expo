c64fee3b4ca5bb22651fa3eeb071cf87de5480dd
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-12-04 03:03:21 +0800

    fixed the home

--- src/app/(camera)/index.tsx
+++ src/app/(camera)/index.tsx
@@ -110,7 +110,7 @@ export default function CameraLeaf() {
       [
         {
           style: "default",
-          text: "Confirm",
+          text: "OK",
           onPress: () => router.back(),
         },
       ]
